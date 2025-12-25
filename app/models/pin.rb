f8d124ff73e6978a74dda224374212ab7a83110a
Diana Osman <osmandyana@gmail.com>
2025-12-25 19:39:49 +0400

    Handle images and PDFs safely in pin partial

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,12 @@
-
 class Pin < ApplicationRecord
-  belongs_to :user
-
-  # Active Storage association
   has_one_attached :image
+
+  validates :image,
+            content_type: [
+              "image/png",
+              "image/jpeg",
+              "image/webp",
+              "application/pdf"
+            ],
+            size: { less_than: 10.megabytes }
 end
